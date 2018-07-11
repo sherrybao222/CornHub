8b4ca805345a561ea00f97c924182b456789df46
neetsaki <1366509555qq@sina.com>
2018-07-11 19:57:53 +0800

    diff0.m

---
+++ diff0.m
@@ -0,0 +1,19 @@
+function [tra] = diff0(a,b,ori)
+%UNTITLED4 此处显示有关此函数的摘要
+%   此处显示详细说明
+couple=[a b];
+
+if strcmp(couple,'at')==1 | strcmp(couple,'ta')==1
+    ori(1)=ori(1)+1;
+elseif strcmp(couple,'cg')==1 | strcmp(couple,'gc')==1
+    ori(2)=ori(2)+1;
+elseif strcmp(couple,'ag')==1 | strcmp(couple,'ga')==1 | strcmp(couple,'aa')==1 | strcmp(couple,'gg')==1   
+    ori(3)=ori(3)+1;
+elseif strcmp(couple,'tc')==1 | strcmp(couple,'tt')==1 | strcmp(couple,'cc')==1 | strcmp(couple,'ct')==1   
+    ori(4)=ori(4)+1;
+elseif strcmp(couple,'ac')==1 | strcmp(couple,'tg')==1 | strcmp(couple,'ca')==1 | strcmp(couple,'gt')==1   
+    ori(5)=ori(5)+1;
+end
+tra=ori;
+end
+
